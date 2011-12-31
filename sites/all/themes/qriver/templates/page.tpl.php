ff342c98140565c9129f449e3ccc6102e7a3c53e
Daniel Carter <daniel.carter@ddotb.com>
2011-12-30 23:35:37 -0500

    Basic Style Setup
    1: Header Styles complete
    2: Page structure styles are there, though we still need to style the
    sidebars.
    3: Styled the footer.
    4: Started the background sections.

--- sites/all/themes/qriver/templates/page.tpl.php
+++ sites/all/themes/qriver/templates/page.tpl.php
@@ -106,15 +106,23 @@
             </div>
         </div> <!-- /.section, /#header -->
 
-        <?php if ($breadcrumb): ?>
-            <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-        <?php endif; ?>
+        <div id="hero">
+            <?php if ($page['hero']): ?>
+                <div class="section">
+                    <?php print render($page['hero']) ?>
+                </div>
+            <?php endif; ?>
+        </div>
 
-        <?php print $messages; ?>
+
 
         <div id="main-wrapper">
             <div id="main" class="clearfix">
+                <?php if ($breadcrumb): ?>
+                    <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+                <?php endif; ?>
 
+                <?php print $messages; ?>
                 <?php if ($page['sidebar_first']): ?>
                     <div id="sidebar-first" class="column sidebar">
                         <div class="section">
@@ -127,7 +135,7 @@
                         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
                         <a id="main-content"></a>
                         <?php print render($title_prefix); ?>
-                        <?php if ($title && !$is_front): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+                        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
                         <?php print render($title_suffix); ?>
                         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
                         <?php print render($page['help']); ?>
